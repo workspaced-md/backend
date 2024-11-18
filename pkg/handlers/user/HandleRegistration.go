67322d2eee269c0723ae915ae31bf6e557709912
Arnav Surve <arnav@surve.dev>
2024-11-17 23:13:34 -0800

    password hashing

--- pkg/handlers/user/HandleRegistration.go
+++ pkg/handlers/user/HandleRegistration.go
@@ -7,6 +7,7 @@ import (
 	"github.com/arnavsurve/workspaced/pkg/db"
 	"github.com/arnavsurve/workspaced/pkg/shared"
 	"github.com/labstack/echo/v4"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func HandleNewUser(c echo.Context, store *db.Store) error {
@@ -16,6 +17,14 @@ func HandleNewUser(c echo.Context, store *db.Store) error {
 		log.Println(err)
 		return c.JSON(http.StatusBadRequest, map[string]string{"message": "Failed to create user: " + err.Error()})
 	}
+
+	// hash the password
+	hash, err := bcrypt.GenerateFromPassword([]byte(account.Password), bcrypt.DefaultCost)
+	if err != nil {
+		log.Println(err)
+		return c.JSON(http.StatusBadRequest, map[string]string{"message": "Failed to create user: " + err.Error()})
+	}
+	account.Password = string(hash)
 
 	err = store.CreateAccount(&account)
 	if err != nil {
